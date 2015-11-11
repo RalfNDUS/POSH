179cfb4d8ca34bffc73f7ebd07b5d1360b8901d2
RalfNDUS <ralf.nelle@gmail.com>
2015-11-11 18:16:43 +0100

    ok

--- SoftwareInv/SoftwareInv/GetSoftwareInventory.ps1
+++ SoftwareInv/SoftwareInv/GetSoftwareInventory.ps1
@@ -5,28 +5,42 @@
 # 	Last Change: 09.11.2015
 #
 
-param([string[]]$ComputerName,[switch]$CleanOnly, [switch]$ShowCleanedProducts)
+param(
+	[string[]]$ComputerName,
+	[string]$HostsFile = "$($pwd.path)\hosts.txt",
+	[string]$XmlFile = ("{0}\SwInv_{1}.xml" -f $pwd.path,$ENV:USERDNSDOMAIN.replace('.','_')),
+	[switch]$CleanOnly, 
+	[switch]$ShowCleaningReport,
+	[switch]$ShowHosts
+)
 
 # ---------------------------------------------------------------------------
 #     Settings
 
-$ComputersFile = $pwd.path + "\hosts.txt"
-$XmlFile = $pwd.path + "\SoftwareInv.xml"
-$XmlCleanFile = $pwd.path + "\SoftwareInv_cleaned.xml"
-$PatternFile = $pwd.path + "\CleanPatterns.txt"
-$CleaningReportFile = $pwd.path + "\CleaningReport.txt"
+$XmlFile = "{0}\SwInv_{1}.xml" -f $pwd.path,$ENV:USERDNSDOMAIN.replace('.','_')
+$XmlCleanFile = $XmlFile.replace(".xml","_Cleaned.xml")
+$PatternFile = "$($pwd.path)\CleaningPatterns.txt"
+$CleaningReportFile = "$($pwd.path)\CleaningReport.txt" 
 
 # prepare xml store
 [Xml]$Xml = @'
 <?xml version="1.0" encoding="iso-8859-1"?>
 <Inventory>
-  <Computers />
+	<Computers />
 </Inventory>
 '@
 
 
 # ---------------------------------------------------------------------------
 #     Functions
+
+# removes remarks
+Filter Get-NameFromLine { 
+  if (($_.trim() -eq "") -or $_.StartsWith('#')) { 
+  } else {
+	if ($_.contains('#')) { $_.split('#')[0].trim() } else { $_ } 
+  }
+}
 
 # ---------------------------------------------------------------------------
 Function New-XmlElement($XmlParent, $Name, $Child, $InnerText, $Attribute ) {
@@ -64,9 +78,63 @@ Function Get-Operatingsystem {
 			Write-Host "Query operatingsystem on '$Computer' ... " -nonewline -fore gray		
 		
 			try { 
-				$Result = gwmi -q "SELECT Caption FROM Win32_Operatingsystem" -ComputerName $Computer -ErrorAction Stop
-				New-Object -TypeName PSCustomObject -Property @{
-				  'ComputerName' = $Computer; 'Name' = $result.Caption
+				$OS = gwmi -q "SELECT Caption FROM Win32_Operatingsystem" -ComputerName $Computer -ErrorAction Stop
+				$CS = gwmi -q "SELECT Domain,Manufacturer,Model,UserName FROM Win32_Computersystem" -ComputerName $Computer -ErrorAction Stop
+				$Enc = gwmi -q "SELECT SerialNumber FROM Win32_SystemEnclosure" -ComputerName $Computer -ErrorAction Stop
+				$serial = $Enc.SerialNumber
+				if ($Enc.SerialNumber.trim() -eq "") {
+					$Board = gwmi -q "SELECT SerialNumber FROM Win32_BaseBoard" -ComputerName $Computer -ErrorAction Stop
+					$serial = $Board.SerialNumber
+				}
+				
+				$props = [ordered]@{
+				  'ComputerName' = $Computer
+				  'Domain' = $CS.Domain
+				  'Name' = $OS.Caption
+				  'Manufacturer' = $CS.Manufacturer
+				  'Model' = $CS.Model
+				  'SerialNumber' = $Serial
+				  'LastUser' = $CS.UserName
+				}
+				New-Object PSObject -prop $props
+				
+				Write-Host "done." -fore gray			
+
+			}
+			catch { 
+				Write-Host
+				Write-Host "$($Computer): $($_.Exception.Message)" -fore red -back black 
+			} 		
+		}
+	}
+}
+
+# ---------------------------------------------------------------------------
+Function Get-SQLServerVersion {
+    [CmdletBinding(SupportsShouldProcess=$true)]
+    param(
+        [Parameter(ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true, Position=0)]
+        [string[]]$ComputerName = $env:COMPUTERNAME
+    )
+
+    process {
+		$NameSpace = "root\Microsoft\SqlServer\ComputerManagement10"
+	
+        foreach ($Computer in $ComputerName) {
+		
+			Write-Host "Query SQL Server version on '$Computer' ... " -nonewline -fore gray		
+		
+			try { 
+				
+				$WmiSql = gwmi -Namespace $NameSpace -Class SqlServiceAdvancedProperty -ComputerName $Computer -ErrorAction Stop | 
+					?{ "VERSION","SKUNAME" -contains $_.PropertyName } | `
+					Select ServiceName,PropertyName,PropertyStrValue | group ServiceName -AsHashTable -AsString
+				
+				foreach ($item in $WmiSql.Keys) { 
+					New-Object -TypeName PSCustomObject -Property @{ 
+					  'ComputerName' = $Computer;
+					  'SQLVersion' = ($WmiSql[$item] | %{  $_.PropertyStrValue }) -join " "
+					}				
 				}
 			}
 			catch { Write-Host "$($Computer): $($_.Exception.Message)" -fore red -back black } 		
@@ -75,6 +143,8 @@ Function Get-Operatingsystem {
 		}
 	}
 }
+
+
 
 # ---------------------------------------------------------------------------
 Function Get-RemoteSoftware ($ComputerName = $ENV:COMPUTERNAME) {
@@ -108,7 +178,7 @@ Function Get-RemoteSoftware ($ComputerName = $ENV:COMPUTERNAME) {
 					$version = $regProv.GetStringValue($HKLM,(join-path $UNINSTALL_KEY $subkey), "DisplayVersion").sValue;
 					if ($version) { $name = ("{0} [{1}]" -f $name.trim(),$version.trim()) }
 
-					New-Object -TypeName PSCustomObject -Property @{
+					New-Object -TypeName PSCustomObject -Property @{ 
 					  'ComputerName' = $Computer;
 					  'AppName' = $name.trim();
 					  'Vendor' = $regProv.GetStringValue($HKLM,(join-path $UNINSTALL_KEY $subkey), "Publisher").sValue;
@@ -151,11 +221,23 @@ Function Clean-InventoryData {
 
 # ---------------------------------------------------------------------------
 Function Show-CleanedProducts {
-	Compare-Object (gc .\SoftwareInv.xml) (gc .\SoftwareInv_cleaned.xml) | %{ 
+	Compare-Object (gc $XmlFile) (gc $XmlCleanFile) | %{ 
 		$_.InputObject.replace("<Software>","").replace("</Software>","").trim() 
 	} | sort | Get-Unique | sc $CleaningReportFile
 	notepad $CleaningReportFile
 }
+
+# ---------------------------------------------------------------------------
+Function Show-Hosts {
+
+	if (!(Test-Path $XmlFile)) {
+		Write-Host "'$XmlFile' not found. Script aborted." -fore red -back black; break
+	}
+	[xml]$Xml = gc $XmlFile
+	$Xml.SelectNodes("//Computer") | sort Name | ft Name,LastUser -AutoSize
+}
+
+
 
 # ---------------------------------------------------------------------------
 Function Get-InventoryData {
@@ -164,11 +246,11 @@ Function Get-InventoryData {
 	if ($ComputerName) { 
 		[string[]]$Computers = $ComputerName 
 	} else { 
-		if (!(Test-Path $ComputersFile)) {
-			Write-Host "'$ComputersFile' not found. Using localhost." -fore yellow
+		if (!(Test-Path $HostsFile)) {
+			Write-Host "'$HostsFile' not found. Using localhost." -fore yellow
 			[string[]]$Computers = $env:COMPUTERNAME
 		} else {
-			[string[]]$Computers = gc $ComputersFile | ?{ $_ -notlike "#*" }
+			[string[]]$Computers = gc $HostsFile | Get-NameFromLine
 		}
 	}
 
@@ -190,6 +272,11 @@ Function Get-InventoryData {
 		# add computer node
 		$AvailableComputers += $Result.ComputerName
 		$XmlComputer = New-XmlElement $XmlComputers -Name "Computer" -Attribute @{ 'Name' = $Result.ComputerName }
+		$XmlComputer.SetAttribute('Domain',$Result.Domain)
+		$XmlComputer.SetAttribute('Model',$Result.Manufacturer + ' ' + $Result.Model)	
+		$XmlComputer.SetAttribute('SerialNumber',$Result.SerialNumber)
+		$XmlComputer.SetAttribute('LastUser',$Result.LastUser)			
+			
 		New-XmlElement $XmlComputer -Name 'Software' -InnerText $Result.Name | Out-Null
 
 		# add timestamp
@@ -218,7 +305,8 @@ Function Get-InventoryData {
 # ===========================================================================
 #   MAIN
 
-if ($ShowCleanedProducts) { Show-CleanedProducts; break; }
+if ($ShowCleaningReport) { Show-CleanedProducts; break; }
+if ($ShowHosts) { Show-Hosts; break; }
 
 if (!($CleanOnly)) { Get-InventoryData }
 	
